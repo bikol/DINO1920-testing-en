2191b9bd46f49963378d71b403533894ce80d562
Aybars <42347579+aybarsyildiz@users.noreply.github.com>
2019-11-18 15:47:06 +0100

    test2
    limits

--- UltraMultiplication/src/main/java/wmi/um/Multiply.java
+++ UltraMultiplication/src/main/java/wmi/um/Multiply.java
@@ -13,7 +13,7 @@ public class Multiply {
     public static String multiply(String a, String b){
         int aa = Integer.parseInt(a);
         int bb = Integer.parseInt(b);
-        if( aa<100 && bb<100){
+        if( aa<500 && bb<500){ //changed the limits for the second test
             return Integer.toString(aa+bb);
         }
         throw new IllegalArgumentException();
